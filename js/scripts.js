9107f4f358f0983827ca1796bcb4c6679ea0f7a8
Alexandra Holcombe and sandro mijatovic <alexandra@>
2017-01-19 16:35:51 -0800

    Create functions to loop through input repeatedly to return newly-ordered array

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,32 @@
+//Business Logicus
+var userInput;
+var finalArray = [];
+var innerArray = [];
+
+var lowerCase = function(input) {
+  input = input.toLowerCase();
+  return input;
+};
+
+var length = function(input) {
+  innerArray = [];
+  var sqRoot = Math.sqrt(input.length);
+  for (j = 0; j < sqRoot; j++) {
+    for (i = j; i < input.length; i += sqRoot){
+      innerArray.push(input.charAt(i));
+    };
+  };
+  return(innerArray);
+};
 
 
 
+//User Interface Logic
 $(function() {
   $("form").submit(function() {
     event.preventDefault();
-    var userInput = $("#user-input").val();
-    $("#result").text(userInput);
+    userInput = $("#user-input").val();
+    $("#result").text(length(userInput));
 
   })
 })
